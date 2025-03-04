a9aa0d01ade33ce127e822dc30593385ee5158a8
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2025-03-03 22:58:07 -0500

    rm worldtimeapi

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,8 +2,8 @@ export async function POST(request: Request) {
   const body = await request.json();
   const ip = body.ip;
 
-  const time_api = await fetch("http://worldtimeapi.org/api/ip/" + ip);
-  const time_data = await time_api.json();
+  //const time_api = await fetch("http://worldtimeapi.org/api/ip/" + ip);
+  //const time_data = await time_api.json();
 
   const geo_api = await fetch("https://freegeoip.app/json/" + ip);
   const geo_data = await geo_api.json();
@@ -12,11 +12,11 @@ export async function POST(request: Request) {
   const quote_data = await quote_api.json();
 
   const bodyResponse = {
-    abbreviation: time_data.abbreviation,
-    timezone: time_data.timezone,
-    day_of_week: time_data.day_of_week,
-    day_of_year: time_data.day_of_year,
-    week_number: time_data.week_number,
+    //abbreviation: time_data.abbreviation,
+    //timezone: time_data.timezone,
+    //day_of_week: time_data.day_of_week,
+    //day_of_year: time_data.day_of_year,
+    //week_number: time_data.week_number,
     city_name: geo_data.city,
     country_name: geo_data.country_name,
     content: quote_data.content,
