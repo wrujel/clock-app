952e34c54d962d1ff0b32a757ed7c487162a1b4f
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2025-03-03 23:12:41 -0500

    rm quote api

--- app/api/quote/route.ts
+++ app/api/quote/route.ts
@@ -1,6 +1,6 @@
 export async function GET(request: Request) {
-  const quote = await fetch("https://api.quotable.io/random");
-  const data = await quote.json();
-  const response = { content: data.content, author: data.author };
+  //const quote = await fetch("https://api.quotable.io/random");
+  //const data = await quote.json();
+  const response = { content: "", author: "" };
   return new Response(JSON.stringify(response), { status: 200 });
 }
